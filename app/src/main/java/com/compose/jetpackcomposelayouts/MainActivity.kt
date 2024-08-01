ee93c9582cf738969c12dac217ee61a38ac8b222
Yco Silvallana <ycosilvallana@gmail.com>
2024-08-01 23:59:03 +0800

    Initial commit

---
+++ app/src/main/java/com/compose/jetpackcomposelayouts/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.compose.jetpackcomposelayouts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.compose.jetpackcomposelayouts.ui.theme.JetpackComposeLayoutsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeLayoutsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // TODO add contents here
+                }
+            }
+        }
+    }
+}
+
